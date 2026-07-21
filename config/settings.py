60606a44deef5c51247a06bb2f77c098d48108e3
sarsenbaizeinolla-hub <sarsenbai.zeinolla@mail.ru>
2026-07-21 23:01:55 +0600

    Fix context processors typo

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.make_context', # если было, или стандартное
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
