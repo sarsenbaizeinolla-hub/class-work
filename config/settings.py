0736cf0a1dc94c0ae778d272338e10f21c3478bc
sarsenbaizeinolla-hub <sarsenbai.zeinolla@mail.ru>
2026-07-21 22:46:44 +0600

    Fix STATIC_URL slash

--- config/settings.py
+++ config/settings.py
@@ -106,7 +106,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (загружаемые картинки машин и т.д.)
